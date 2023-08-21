d95b7858f1bef5ecb2e3d43527a5cbce8684e146
tubring25 <tubring25@gmail.com>
2023-08-21 23:41:31 +0900

    reset

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+};
+const withNextra = require("nextra")({
+  theme: "nextra-theme-blog",
+  themeConfig: "./theme.config.tsx",
+});
+module.exports = withNextra(nextConfig);
